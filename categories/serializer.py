34a69574ee7b8ba0e41ca93da0407f6b5dbf9b0c
RyuMinSu <sonicsu94@gmail.com>
2023-08-19 17:43:48 +0900

    room list and detail serializers

--- categories/serializer.py
+++ categories/serializer.py
@@ -5,7 +5,7 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ("name", "kind")
         # fields = ("name", "kind")
         # exclude = ("create_at")
         
