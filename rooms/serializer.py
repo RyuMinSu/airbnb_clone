d9cfce2c135357ef44958421bbfab8373b4985e4
RyuMinSu <sonicsu94@gmail.com>
2023-08-23 21:20:32 +0900

    room SerializerMethodField

--- rooms/serializer.py
+++ rooms/serializer.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Amenity, Room
 from users.serializer import TinyUserSerializer
 from categories.serializer import CategorySerializer
@@ -14,10 +14,16 @@ class RoomDetailSerializer(ModelSerializer):
     owner = TinyUserSerializer(read_only=True)
     amenities = AmenitySerializer(many=True, read_only=True)
     category = CategorySerializer(read_only=True)
+    rating = SerializerMethodField()
 
     class Meta:        
         model = Room
         fields = "__all__"
+
+    def get_rating(self, room):        
+        return room.rating()
+        
+
 
     # def create(self, validated_data):
     #     print(validated_data)
@@ -25,7 +31,11 @@ class RoomDetailSerializer(ModelSerializer):
 
         
 class RoomListSerializer(ModelSerializer):
+    rating = SerializerMethodField()
     class Meta:
         model = Room
-        fields = ("id", "country", "city", "price",)
+        fields = ("id", "country", "city", "price", "rating")
+
+    def get_rating(self, room):
+        return room.rating()
 
