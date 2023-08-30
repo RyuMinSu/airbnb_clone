e44e004be99168545f22816c251f5d198f8fdc38
RyuMinSu <sonicsu94@gmail.com>
2023-08-30 22:20:40 +0900

    experience detail serializer

--- experiences/views.py
+++ experiences/views.py
@@ -4,7 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
-from rest_framework.exceptions import ParseError
+from rest_framework.exceptions import ParseError, PermissionDenied
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from categories.models import Category
 from .serializer import PerkSerializer, ExperienceSerializer, ExperienceDetailSerializer
 from .models import Perk, Experience
@@ -53,6 +54,8 @@ class PerkDetail(APIView):
 
 
 class ExperienceList(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         experiences = Experience.objects.all()
         serializer = ExperienceSerializer(
@@ -62,7 +65,7 @@ class ExperienceList(APIView):
         )
         return Response(serializer.data)
     
-    def post(self, request):
+    def post(self, request):        
         serializer = ExperienceDetailSerializer(data=request.data)
         if serializer.is_valid():
             #----- category
@@ -94,13 +97,60 @@ class ExperienceList(APIView):
 
 
 class ExperienceDetail(APIView):
-    def get_experience(self, pk):
+   permission_classes = [IsAuthenticatedOrReadOnly]
+
+   def get_experience(self, pk):
         try:
             return Experience.objects.get(pk=pk)
         except Experience.DoesNotExist:
             raise NotFound
-
-    def get(self, request, pk):
+   
+   def get(self, request, pk):
         experience = self.get_experience(pk)
         serializer = ExperienceDetailSerializer(experience, context={"request":request})
         return Response(serializer.data)
+
+   def delete(self, request, pk):            
+      experience = self.get_experience(pk)
+      if experience.host != request.user:
+          raise PermissionDenied
+      experience.delete()
+      return Response(status=HTTP_204_NO_CONTENT)
+   
+   def put(self, request, pk):
+      experience = self.get_experience(pk)      
+      if experience.host != request.user:
+          raise PermissionDenied
+      
+      serializer = ExperienceDetailSerializer(experience, data=request.data, partial=True)
+      if serializer.is_valid():
+         #----- category
+         category_pk = request.data.get("category")
+         if category_pk:
+            try:
+               category = Category.objects.get(pk=category_pk)
+               if category.kind == Category.CategoryKindChoices.ROOMS:
+                  raise ParseError("Experience만 됨")
+            except Category.DoesNotExist:
+               raise ParseError("카테고리없음")
+         try:
+            with transaction.atomic():
+               if category_pk == None:
+                  new_experience = serializer.save(host=request.user)
+               else:
+                  new_experience = serializer.save(host=request.user, category=category)
+               #----- explanation
+               explanation_pks = request.data.get("explanations")
+               if explanation_pks:
+                  new_experience.explanations.clear()
+                  for explanation_pk in explanation_pks:                        
+                     explanation = Perk.objects.get(pk=explanation_pk)
+                     new_experience.explanations.add(explanation)
+               serializer = ExperienceDetailSerializer(new_experience, context={"request":request})
+               return Response(serializer.data)
+         except Perk.DoesNotExist:
+            raise ParseError("없는 explanation")
+      else:
+         return Response(serializer.errors)
+
+          
