f4530ffdea068cf6aba5f9dbb7b0d92c43239f94
RyuMinSu <sonicsu94@gmail.com>
2023-09-01 23:41:11 +0900

    post amenity test

--- rooms/tests.py
+++ rooms/tests.py
@@ -6,12 +6,13 @@ from . import models
 class TestAmenities(APITestCase):
 	NAME = "Amenity Test"
 	DESC = "Amenity Description"
+	URL = "/api/v1/rooms/amenities"
 
 	def setUp(self):
 		models.Amenity.objects.create(name=self.NAME, description=self.DESC,)
 	
 	def test_all_amenities(self):
-		response = self.client.get("/api/v1/rooms/amenities")
+		response = self.client.get(self.URL)
 		data = response.json()
 		print(data)
 		# get으로 접속 되는지
@@ -24,3 +25,26 @@ class TestAmenities(APITestCase):
 		self.assertEqual(data[0]["name"], self.NAME)
 		# data.description == self.DESC
 		self.assertEqual(data[0]["description"], self.DESC)
+
+	def test_post_amenities(self):
+		new_amenity = "new amenity"
+		new_desc = "new description"
+
+		#----- 데이터를 넣은상태
+		response = self.client.post(self.URL, data={"name": new_amenity, "description": new_desc})
+		data = response.json()
+		# post가 잘 동작 하는지
+		self.assertEqual(response.status_code, 200, "not 200")
+		# 들어간 데이터와 보낸 데이터가 맞는지
+		self.assertEqual(data["name"], new_amenity,)
+		self.assertEqual(data["description"], new_desc,)
+
+		#------ 데이터를 넣지 않은 상태 -> serializer.errors, badrequest
+		response = self.client.post(self.URL)
+		data = response.json()
+		# post가 잘 동작 하는지(badrequest)
+		self.assertEqual(response.status_code, 400,)
+		# 에러문구 나오는지 (serializer.errors)
+		self.assertIn("name", data)
+
+
