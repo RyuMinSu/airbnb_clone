778e1846a3738fc0608f63414413febe52ccb44f
RyuMinSu <sonicsu94@gmail.com>
2023-09-22 17:46:25 +0900

    for github login

--- users/views.py
+++ users/views.py
@@ -114,19 +114,53 @@ class JWTLogIn(APIView):
 			return Response({"error": "wrong password"})
 		
 class GithubLogin(APIView):
-	def post(self, request):
-		print(request.data)
-		code = request.data.get("code")
-		access_token = requests.post(f"https://github.com/login/oauth/access_token?code={code}&client_id=74a7e93ce84cbdf51d31&client_secret={settings.GH_SECRET}", headers={"Accept": "application/json"})
-		access_token = access_token.json().get("access_token")
-		user_data = requests.get(
-			"https://api.github.com/user",
-			headers={
-				"Authorization": f"Bearer {access_token}",
-				"Accept": "application/json",
+	try:
+		def post(self, request):
+			print(request.data)
+			code = request.data.get("code")
+			access_token = requests.post(f"https://github.com/login/oauth/access_token?code={code}&client_id=74a7e93ce84cbdf51d31&client_secret={settings.GH_SECRET}", headers={"Accept": "application/json"})
+			access_token = access_token.json().get("access_token")
+			# ----- user
+			user_data = requests.get(
+				"https://api.github.com/user",
+				headers={
+					"Authorization": f"Bearer {access_token}",
+					"Accept": "application/json",
+					}
+			)
+			user_data = user_data.json()
+			print(user_data)
+
+			# ----- user email
+			user_emails = requests.get(
+				"https://api.github.com/user/emails",
+				headers={
+					"Accept": "application/json",
+					"Authorization": f"Bearer {access_token}"
 				}
-		)
-		print(user_data.json())
+			)
+			user_emails = user_emails.json()
+			print(user_emails)
+
+			try:
+				user = User.objects.get(email=user_emails[0].get("email"))
+				login(request, user)
+				return Response(status=status.HTTP_200_OK)
+			except User.DoesNotExist:
+				user = User.objects.create(
+					username = user_data.get("login"),
+					email = user_emails[0].get("email"),
+					# name = user_data.get("name"),
+					avatar = user_data.get("avatar_url"),
+				)
+				user.set_unusable_password()
+				user.save()
+				login(request, user)
+				return Response(status=status.HTTP_200_OK)
+	except Exception:
+		raise Response(status=status.HTTP_400_BAD_REQUEST)
+
+		
 		
 		
 
